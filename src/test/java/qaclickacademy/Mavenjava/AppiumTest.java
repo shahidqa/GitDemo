336d26d90c1339a0ea11c39dec2dc3e2a645feb8
shahid <smshahid20@gmail.com>
2020-12-31 18:44:47 +0530

    first comit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAppAndriod()
+	{
+	System.out.println("nativeAppAndriod");	
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+		
+	}
+
+
+}
